d13eb712f7831287c9294cf4a9ce9ad9e114d006
neeleshksingh <neelesh1517@gmail.com>
2023-02-10 14:28:02 +0530

    updated with custom id generator

--- backend/model/basic-info.js
+++ backend/model/basic-info.js
@@ -3,13 +3,15 @@ const Schema = mongoose.Schema
 const { ObjectId } = mongoose.Schema.Types;
 
 const basicInfoSchema = new Schema({
+    _id: {
+        type: String,
+        required: true
+    },
     propertyType: { type: String, required: true },
     mobile: { type: String },
     area: { type: Number },
     views: { type: Number, default: Math.floor(Math.random() * 100) },
-    ppd: { type: String, set: function(value) {
-        return 'PPD1' + value;
-    } },
+    ppd: { type: String },
     user: { type: ObjectId, ref: 'User', required: false }
 }, { timestamps: true });
 
