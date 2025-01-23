9c6b24706cb4ce60c5a2289faebd5c222f8355bc
Neelesh Kumar Singh <108358262+neeleshksingh@users.noreply.github.com>
2025-01-23 09:32:11 +0530

    Refactor app.js for improved readability and add a root endpoint

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,21 @@ const express = require('express')
 const connection = require('./connections/connection')
 const userRoutes = require('./routes/property')
 const cors = require('cors')
-const getroutes=require('./routes/getroutes')
+const getroutes = require('./routes/getroutes')
 connection()
 
 const app = express()
 
-app.use(cors())
-app.use(userRoutes)
-app.use('/get',getroutes)
+app.use(cors());
+app.use(userRoutes);
+app.use('/get', getroutes);
+
+app.get("/", (req, res) => {
+    res.send("API IS WORKING")
+});
 
 app.get("*", (req, res) => {
     res.status(404).send("API IS NOT FOUND");
-})
+});
 
-app.listen(3016 || process.env.PORT, () => { console.log("Listening on port 3016") })
+app.listen(3016, () => { console.log("Listening on port 3016") });
